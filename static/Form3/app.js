bacf4f250d3569a7392a0cb360a0f0d39373fb9f
Samuel Storment <sstorme@siue.edu>
2020-03-28 04:46:34 -0500

    Added some drag and drop but it doesn't work exactly as desired yet

--- static/Form3/app.js
+++ static/Form3/app.js
@@ -1,42 +1,42 @@
-// all of this is performed after the page is loaded
+// Set the flex dropdowns to display nothing
 document.addEventListener("DOMContentLoaded", () => { 
-    let newDropdownnContent = document.querySelector("#new-dropdown");
-    newDropdownnContent.style.display = "none";
-    let termDropdownnContent = document.querySelector("#term-dropdown");
-    termDropdownnContent.style.display = "none";
+    document.querySelector("#new-dropdown").style.display = "none";
+    document.querySelector("#term-dropdown").style.display = "none";
 });
 
-// just grabs the number off of an id and returns the number
-function getIdNumber(id) {
-    let array = id.split("-");
-    return array[array.length - 1];
-}
-
+// toggle button
 function toggleActionContent(id) {
 
-    let num = getIdNumber(id);
-    let actionContentContainer = document.querySelector(`#action-content-container-${num}`);
-    let actionTopbar = document.querySelector(`#action-topbar-${num}`);
+    let toggleButton = document.querySelector(`#toggle-button-${id}`);
+    toggleButton.addEventListener("click", e => {
+    
+        let actionContainer = document.querySelector(`#action-container-${id}`);
+        let actionContentContainer = document.querySelector(`#action-content-container-${id}`);
+        let actionTopbar = document.querySelector(`#action-topbar-${id}`);
 
-    if (actionContentContainer.style.display === "none") {
-        actionContentContainer.style.display = "grid";
-        actionTopbar.style.borderBottom = "1px solid gray";
-        actionTopbar.style.borderBottomLeftRadius = "0px";
-        actionTopbar.style.borderBottomRightRadius = "0px";
-    } else {
-        actionContentContainer.style.display = "none";
-        actionTopbar.style.borderBottomLeftRadius = "10px";
-        actionTopbar.style.borderBottomRightRadius = "10px";
-        actionTopbar.style.borderBottom = "none";
-    }
+        if (actionContentContainer.style.display === "none") {
+            actionContentContainer.style.display = "grid";
+            actionTopbar.style.borderBottom = "1px solid gray";
+            actionTopbar.style.borderBottomLeftRadius = "0px";
+            actionTopbar.style.borderBottomRightRadius = "0px";
+        } else {
+            actionContentContainer.style.display = "none";
+            actionTopbar.style.borderBottomLeftRadius = "10px";
+            actionTopbar.style.borderBottomRightRadius = "10px";
+            actionTopbar.style.borderBottom = "none";
+        }
+    });
 }
 
-function deleteAction(id) {
+// Setup the toggle button for the contact container
+toggleActionContent(0);
 
-    let num = getIdNumber(id);
-    let actions = document.querySelector("#actions");
-    let actionContainer = document.querySelector(`#action-container-${num}`);
-    actions.removeChild(actionContainer);
+// delete button
+function deleteAction(id, actions, actionContainer) {
+    let deleteButton = document.querySelector(`#delete-button-${id}`);
+    deleteButton.addEventListener("click", e => {
+        actions.removeChild(actionContainer);
+    });
 }
 
 // scroll the page to the very bottom so that all of the new content is visible
@@ -45,9 +45,9 @@ function scrollToBottom() {
     scrollingElement.scrollTop = scrollingElement.scrollHeight;
 }
 
+// Event listener for new button's dropdown content
 let newActionButton = document.querySelector("#new-button");
 newActionButton.addEventListener("click", e => {
-    
     let dropdownContent = document.querySelector("#new-dropdown");
 
     if (dropdownContent.style.display === "none") {
@@ -56,6 +56,7 @@ newActionButton.addEventListener("click", e => {
     }
 });
 
+// Event listener for term button's dropdown content
 let termButton = document.querySelector("#term-button");
 termButton.addEventListener("click", e => {
     let dropdownContent = document.querySelector("#term-dropdown");
@@ -78,18 +79,21 @@ window.onclick = e => {
 
 }
 
-function changeTerm(term) {
-    let termButton = document.querySelector("#term-button");
-    termButton.value = term;
-} 
+// Event listeners for Spring, Summer, and Fall buttons
+let terms = document.querySelectorAll(".term");
+terms.forEach(term => {
+    term.addEventListener("click", e => {
+        termButton.value = term.id;
+    });
+});
 
-
+// returns the inner html for the appropriate action
 function getActionHtml(action) {
     if (action === "add") {
         return `<div class="action-topbar" id="action-topbar-${contentCounter}">
                     <span>Add</span>
-                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}" onclick="toggleActionContent(this.id)">Toggle</button>
-                    <button type="button" class="delete-button" id="delete-button-${contentCounter}" onclick="deleteAction(this.id)">Delete</button>
+                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}">Toggle</button>
+                    <button type="button" class="delete-button" id="delete-button-${contentCounter}">Delete</button>
                 </div>
                 <div class="action-content-container" id="action-content-container-${contentCounter}">
                     <div class="action-descriptor">Enter new class info</div>
@@ -107,8 +111,8 @@ function getActionHtml(action) {
     else if (action === "change") {
         return `<div class="action-topbar" id="action-topbar-${contentCounter}">
                     <span>Change</span>
-                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}" onclick="toggleActionContent(this.id)">Toggle</button>
-                    <button type="button" class="delete-button" id="delete-button-${contentCounter}" onclick="deleteAction(this.id)">Delete</button>
+                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}">Toggle</button>
+                    <button type="button" class="delete-button" id="delete-button-${contentCounter}">Delete</button>
                 </div>
                 <div class="action-content-container" id="action-content-container-${contentCounter}">
                     <div class="action-descriptor">Enter changes</div>
@@ -126,8 +130,8 @@ function getActionHtml(action) {
     else if (action === "cancel") {
         return `<div class="action-topbar" id="action-topbar-${contentCounter}">
                     <span>Cancel</span>
-                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}" onclick="toggleActionContent(this.id)">Toggle</button>
-                    <button type="button" class="delete-button" id="delete-button-${contentCounter}" onclick="deleteAction(this.id)">Delete</button>
+                    <button type="button" class="toggle-button" id="toggle-button-${contentCounter}">Toggle</button>
+                    <button type="button" class="delete-button" id="delete-button-${contentCounter}">Delete</button>
                 </div>
                 <div class="action-content-container" id="action-content-container-${contentCounter}">
                     <div class="action-descriptor">Enter cancellation reason</div>
@@ -144,18 +148,83 @@ function getActionHtml(action) {
     }
 }
 
-
+// Event listeners for Add, Change, and Cancel buttons
 let contentCounter = 1;
-function createAction(action) {
-    contentCounter++;
-    let actions = document.querySelector("#actions");
-    let actionContainer = document.createElement("div");
+let actions = document.querySelectorAll(".action");
+actions.forEach(action => {
+    action.addEventListener("click", e => {
+        contentCounter++;
+        let actions = document.querySelector("#actions");
+        let actionContainer = document.createElement("div");
 
-    actionContainer.setAttribute("class", "action-container");
-    actionContainer.setAttribute("id", `action-container-${contentCounter}`);
+        actionContainer.setAttribute("class", "action-container draggable");
+        actionContainer.setAttribute("draggable", "true");
+        actionContainer.setAttribute("id", `action-container-${contentCounter}`);
 
-    actionContainer.innerHTML = getActionHtml(action);
-    
-    actions.appendChild(actionContainer);
-    scrollToBottom();
+        actionContainer.innerHTML = getActionHtml(action.id);
+        
+        actions.appendChild(actionContainer);
+
+        toggleActionContent(contentCounter);
+        deleteAction(contentCounter, actions, actionContainer);
+
+        handleDrag(actionContainer);
+
+        scrollToBottom();
+    });
+});
+
+function handleDrag(actionContainer) {
+
+    actionContainer.addEventListener("dragstart", () => {
+        actionContainer.querySelector(".action-content-container").style.display = "none";
+        let topbar = actionContainer.querySelector(".action-topbar");
+        topbar.style.borderBottom = "none";
+        topbar.style.borderBottomLeftRadius = "10px";
+        topbar.style.borderBottomRightRadius = "10px";
+        actionContainer.classList.add("dragging");
+    });
+
+    actionContainer.addEventListener("dragend", () => {
+        let contentContainer = actionContainer.querySelector(".action-content-container");
+        let topbar = actionContainer.querySelector(".action-topbar");
+        // we want to display the contentContainer as grid ONLY if the container was open when we started dragging
+        contentContainer.style.display = "grid";
+        topbar.style.borderBottom = "1px solid gray";
+        topbar.style.borderBottomLeftRadius = "0px";
+        topbar.style.borderBottomRightRadius = "0px";
+        actionContainer.classList.remove("dragging");
+    });
+
+    actions = document.querySelector("#actions");
+
+    actions.addEventListener("dragover", e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(actions, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            actions.appendChild(draggable)
+        } else {
+            actions.insertBefore(draggable, afterElement)
+        }
+    });
+}
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        // normally, midpoint would be calculated as height / 2 but we want to always ue the height of the topbar since we can toggle the box's height 
+        //const midpoint = box.height / 2;
+        const midpoint = 55 / 2; // topbar has a height of 55
+
+        const offset = y - box.top - midpoint;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child};
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
