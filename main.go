b5c647ded758c9dcab0dc846e705e2c510d87588
Samuel Storment <sstorme@siue.edu>
2020-03-29 18:25:29 -0500

    Contact form data is now making its way to the server. Need to add names to other input fields

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"		// Formatting - Nice printing	
 	"net/http"
 	"html/template"
+	"encoding/json"
 )
 
 
@@ -30,6 +31,29 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/add", http.StatusFound)
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	page := Page{ "Add", "Add a Meeting"}
+	temp, err := template.ParseFiles("templates/form4.html")
+
+	if err != nil { fmt.Println("Err", err) }
+
+	temp.Execute(w, page)
+}
+
+func formSubmit(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("Something messed up", err)
+	} else {
+		json, err := json.Marshal(r.PostForm)
+		if err != nil {
+			fmt.Println(err);
+			return
+		}
+		w.Write(json)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hey dudes how we doin")
 }
@@ -39,6 +63,8 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/saveDate", submitHandler)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/submit", formSubmit)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
