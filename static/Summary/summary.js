6cc36f5a5d49fd165120761182be346c27f23ca8
Samuel Storment <sstorme@siue.edu>
2020-04-20 13:26:07 -0500

    Started working on summary

---
+++ static/Summary/summary.js
@@ -0,0 +1,17 @@
+let fullTable = [
+    {type: "Add",    subject: "CS", number: "445", section: "002", CRN: "52030", from: "Robinson, Frank", waiting: "pending dean", season: "Summer", year: 2020, date: "4/12/2020", id: 1, groupId: 1},
+    {type: "Change",    subject: "CS", number: "499", section: "002", CRN: "52189", from: "Gibson, Bob", waiting: "pending chair", season: "Summer", year: 2020, date: "4/12/2020", id: 5, groupId: 1},
+    {type: "Add",    subject: "CS", number: "145", section: "002", CRN: "52739", from: "Brett, George", waiting: "pending dean", season: "Summer", year: 2020, date: "4/12/2020", id: 9, groupId: 1},
+    {type: "Change", subject: "Art", number: "240", section: "001", CRN: "55432", from: "Killebrew, Harmon", waiting: "Approved", season: "Summer", year: 2020, date: "4/08/2020", id: 13, groupId: 5},
+    {type: "Cancel", subject: "Art", number: "200", section: "001", CRN: "55472", from: "Damon, Johnny", waiting: "Approved", season: "Summer", year: 2019, date: "4/08/2019", id: 14, groupId: 5},
+    {type: "Change", subject: "Math", number: "416", section: "001", CRN: "53633", from: "Koufax, Sandy", waiting: "pending chair", season: "Spring", year: 2020, date: "3/25/2020", id: 2, groupId: 2},
+    {type: "Change", subject: "Math", number: "184", section: "001", CRN: "56639", from: "Thomas, Frank", waiting: "pending chair", season: "Spring", year: 2020, date: "3/25/2020", id: 6, groupId: 2},
+    {type: "Change", subject: "Math", number: "284", section: "001", CRN: "51230", from: "McCarver, Tim", waiting: "pending chair", season: "Spring", year: 2020, date: "3/25/2020", id: 10, groupId: 2},
+    {type: "Change", subject: "Phil", number: "324", section: "002", CRN: "51970", from: "Lajoie, Nap", waiting: "pending chair", season: "Fall", year: 2019, date: "9/22/2019", id: 3, groupId: 3},
+    {type: "Change", subject: "Phil", number: "330", section: "002", CRN: "54281", from: "Boggs, Wade", waiting: "pending dean", season: "Fall", year: 2019, date: "9/22/2019", id: 7, groupId: 3},
+    {type: "Change", subject: "Phil", number: "521", section: "002", CRN: "52437", from: "Wagner, Honus", waiting: "pending dean", season: "Fall", year: 2019, date: "9/22/2019", id: 11, groupId: 3},
+    {type: "Cancel", subject: "Eng", number: "323", section: "001", CRN: "52470", from: "Williams, Ted", waiting: "pending chair", season: "Fall", year: 2019, date: "7/08/2019", id: 12, groupId: 4},
+    {type: "Cancel", subject: "Eng", number: "212", section: "001", CRN: "58860", from: "Craig, Allen", waiting: "pending chair", season: "Fall", year: 2019, date: "7/08/2019", id: 8, groupId: 4},
+    {type: "Cancel", subject: "Eng", number: "162", section: "001", CRN: "57764", from: "Kaline, Mickey", waiting: "pending dean", season: "Fall", year: 2019, date: "7/08/2019", id: 4, groupId: 4},
+    {type: "Change", subject: "IS", number: "330", section: "003", CRN: "57904", from: "Buck, Jack", waiting: "Denied", season: "Spring", year: 2019, date: "3/23/2019", id: 15, groupId: 6}
+]
